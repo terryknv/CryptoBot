46a50486a558e7a47ada3b1197110953af98557b
Terry <76404591+1Terryy@users.noreply.github.com>
2021-09-11 14:24:23 +0300

    Creating main.py file

---
+++ main.py
@@ -0,0 +1,18 @@
+import discord
+from discord.ext import commands
+
+client = commands.Bot(command_prefix = ">")
+client.remove_command("help")
+
+@client.event
+async def on_ready():
+    print("{client.user} is online.")
+    
+@client.command()
+async def ping(ctx):
+    msg = await ctx.send("Pinging...")
+    await msg.edit(content=f"Ping! `{round(client.latency * 1000)}ms`")
+
+client.load_extension(f'cogs.rep')
+
+client.run("{TOKEN}")
