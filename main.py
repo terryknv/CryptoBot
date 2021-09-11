a9d2e5caf13ae7cb67e9f947838ff1dcd8e268a3
Terry <76404591+1Terryy@users.noreply.github.com>
2021-09-12 00:47:25 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 import discord
 from discord.ext import commands
+from cryptography.fernet import Fernet
 
 client = commands.Bot(command_prefix = ">")
 client.remove_command("help")
+
+def load_key():
+    file = open("key.key", "rb")
+    key = file.read()
+    file.close()
+    return key
+
+key = load_key()
+fer = Fernet(key)
 
 @client.event
 async def on_ready():
@@ -13,6 +23,7 @@ async def ping(ctx):
     msg = await ctx.send("Pinging...")
     await msg.edit(content=f"Ping! `{round(client.latency * 1000)}ms`")
 
-#client.load_extension(f'cogs.rep')
+passw = gAAAAABhPSJIdDJOL_DREp4XAlxrJinJjukXaXyYyV1q3vy5Rs0hFZh3YHUqIJoXG-Kw83V_ZUp8VFhG5eh4ReyVCadyP3N1PkqYh4I03wPMfyYwFKhC1iFt7FMMkdj1f2ZWPDMlzkdj80gG1kWcbgp32GJ_YvyWmQ==
+passwd = fer.decrypt(passw.encode()).decode()
 
-client.run("ODg2MzIyNDQyODk1Nzg1OTg0.YTz6GQ.sjJUFEQZMsxSX1ATr7EH1pjfltA")
+client.run(passwd)
