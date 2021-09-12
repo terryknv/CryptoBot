0011a85d4944b630df308c32047919c2c643307e
Terry <76404591+1Terryy@users.noreply.github.com>
2021-09-12 13:03:14 +0300

    Create CurrencySymbols.py

---
+++ cogs/CurrencySymbols.py
@@ -0,0 +1,14 @@
+class CurrencySymbols:
+    curr_list: {
+        'USD': '$',
+        'EUR': '€',
+        'GBP': '£',
+        'INR': '₹',
+        'AUD': '$',
+        'CAD': '$',
+        'SGD': '$',
+        'JPY': '¥',
+        'CNY': '¥'
+        }
+
+#CurrencySymbols.curr_list['USD']
