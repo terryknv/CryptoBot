f3c2537453f86d2d58f012385936efbb3b5dade6
Terry <76404591+1Terryy@users.noreply.github.com>
2021-09-12 10:00:31 +0300

    Update main.py

--- main.py
+++ main.py
@@ -26,6 +26,7 @@ async def ping(ctx):
     await msg.edit(content=f"Ping! `{round(client.latency * 1000)}ms`")
     
 client.load_extension(f'cogs.cryptoPrice')
+client.load_extension(f'cogs.cryptoConvert')
 
 passw = 'gAAAAABhPSJIdDJOL_DREp4XAlxrJinJjukXaXyYyV1q3vy5Rs0hFZh3YHUqIJoXG-Kw83V_ZUp8VFhG5eh4ReyVCadyP3N1PkqYh4I03wPMfyYwFKhC1iFt7FMMkdj1f2ZWPDMlzkdj80gG1kWcbgp32GJ_YvyWmQ=='
 passwd = fer.decrypt(passw.encode()).decode()
