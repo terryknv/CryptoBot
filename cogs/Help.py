1e96d51716c8f5a769fa42497ce862b9d2b594b9
Terry <76404591+1Terryy@users.noreply.github.com>
2021-09-12 23:03:00 +0300

    Update Help.py

--- cogs/Help.py
+++ cogs/Help.py
@@ -7,7 +7,7 @@ class Help(commands.Cog):
 
     @commands.command()
     async def help(self, ctx):
-        em = discord.Embed(description="Crypto Manager Commands", colour=0xf7931a)
+        em = discord.Embed(colour=0xf7931a)
         em.set_author(name="Crypto Manager Commands", icon_url="https://cdn.pixabay.com/photo/2013/12/08/12/12/bitcoin-225079_960_720.png")
         em.add_field(name="Price Command", value=">btc, >eth, >ltc all return the price of the crypto, you can use the >price <crypto> to check any crypto.")
         em.add_field(name="Convert Command", value=">convert <amount> <currency> <crypto> converts a currency to a crypto or another currency")
