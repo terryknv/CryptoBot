0072192b6bc9ef9750229e72f7fc2a3e7b5a6850
Terry <76404591+1Terryy@users.noreply.github.com>
2021-09-12 09:39:19 +0300

    Update cryptoConvert.py

--- cogs/cryptoConvert.py
+++ cogs/cryptoConvert.py
@@ -7,3 +7,21 @@ class Help(commands.Cog):
         self.client = client
 
         
+    @commands.command(aliases=["usdtobtc", "usd2btc"])
+    async def usdbtc(self, ctx, message):
+
+        r = requests.get(
+            "https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD"
+        )
+
+        r = r.json()
+        usd = r["USD"]
+        index = 1 / usd
+        amount = int(message)
+        converted = amount * index
+        em = discord.Embed(description=f"**${amount}** = **{converted} BTC**")
+        em.set_author(
+            name="Currency Conversion",
+            icon_url="https://cdn.pixabay.com/photo/2013/12/08/12/12/bitcoin-225079_960_720.png",
+        )
+        await ctx.send(embed=em)
