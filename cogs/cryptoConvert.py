4d9b1994f503354116f856ce1de6ea0292352021
Terry <76404591+1Terryy@users.noreply.github.com>
2021-09-12 10:04:27 +0300

    Update cryptoConvert.py

--- cogs/cryptoConvert.py
+++ cogs/cryptoConvert.py
@@ -2,7 +2,7 @@ import discord
 from discord.ext import commands
 import requests
 
-class Help(commands.Cog):
+class cryptoConvert(commands.Cog):
     def __init__(self, client):
         self.client = client
 
