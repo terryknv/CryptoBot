a12caf14e60f4f0018964d29f05630ca44130052
Terry <76404591+1Terryy@users.noreply.github.com>
2021-09-13 10:15:46 +0300

    Update ConfirmationChecker.py

--- cogs/ConfirmationChecker.py
+++ cogs/ConfirmationChecker.py
@@ -26,7 +26,7 @@ class ConfirmationChecker(commands.Cog):
                 continue
         
     @check.error
-    async def check_error(error):
+    async def check_error(ctx, error):
         if isinstance(error, commands.AssertionError):
             await ctx.send('Invalid transaction ID, please make sure it\'s correct.')
                     
